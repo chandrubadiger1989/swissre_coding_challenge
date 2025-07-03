78bc9d2bf5bcd16f5a0415daffe2afe7d9fe0d23
Badiger <Chandrashekhar.Badiger@ros.com>
2025-07-03 14:28:01 +0530

    Logic updated for queries

--- organization-service/README.md
+++ organization-service/README.md
@@ -1,5 +1,22 @@
-The project aims to read the Company employee data from given CSV file and compute the queries given.
+Goal: This project aims to read the Organization Employee data from given CSV file and compute the queries given.
+
+Solution Approach followed:
+- The data in CSV file is Organization hierarchy. can be represented as Tree Structure.
+- I have used the BFS (Breadth First Search) traversal using the Queue data structure, starting from CEO as root.
+- I have created few lists to track the distance of that employee from CEO.
+- Initially CEO will be added to the Queue. Once we pull CEO from q, will add CEO's reportees. All immediate reportees of CEO distance will be tracked as distance of 1.
+- While I am looping through the reportees, I am also checking their average salary, and comparing it with Salary of Manager.
+- If the salary is not ecxpected, then addin to final resulting lists (managerLessThanExptSalary, managerMoreThanExptSalary) 
+- If the disatnce of any employee to CEO is more than 4, adding those employees to final resulting list (empsMoreThanExptReportees)
 
 Below are the assumptions for the project:
 1. Employee Salary is assumed to be within 2 billion (including CEO's, so I am assuming data type of Integer
-2. 
+2. CSV File will be correct format, it will have headers and values for each non blank row
+3. Only CEO will have blank value for Manager, all other Employees will have complete details in the CSV. Otherwise Application will throw Empty fields expected since they are required
+
+Confgurations:
+
+
+How to run:
+- Option 1: This project can be imported into IDE using options like "Open Project" or "Import Maven project", and run the project a Java Apllication. Or, from IDE< right click on "com.swissre.organizationservice.App.java" class and "run as Java application".
+- Option 2: Build the jar file using "mvn clean install", which will generate jar file. Then run the jar file using "java -jar <jar_file_path>"
--- organization-service/src/main/java/com/swissre/organizationservice/App.java
+++ organization-service/src/main/java/com/swissre/organizationservice/App.java
@@ -2,7 +2,9 @@ package com.swissre.organizationservice;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,39 +16,130 @@ import com.swissre.organizationservice.entity.Employee;
 
 /**
  * Application to Improve the Organization Structure, in terms of Average Salary Checks, Reportees length
- *
  */
 public class App 
 {
-	//Store the raw data read from CSV file
+	//Configuration: CSV File Path
+	private static final String CONFIG_CSV_PATH = "C:\\Chandru\\Official\\swissre_coding_challenge\\organization-service\\src\\main\\java\\com\\swissre\\organizationservice\\company_org_structure.csv";
+	//Configuration to check if the manager's salary is at least 20% more than average salary of employees. Configuration can be externalized as well, as well as Environmentalized as well.
+	private static final int CONFIG_MIN_PRCT = 20;
+	//configuration to check if the manager's salary is  at most 50% more than average salary of employees
+	private static final int CONFIG_MAX_PRCT = 50;
+	//Configuration to check if the Employee is having more than 4 reportees in between from CEO and employee
+	private static final int CONFIG_MAX_REPORTEES = 4;
+	
+	//To store the raw data from CSV file
 	private static List<Employee> rawData; 
 	
-	//Store the raw data into Employee Map
+	//To store the raw data into Employee Map for faster access
 	private static Map<Integer, Employee> empMap;
 	
+	//Capturing starting point, i.e ID of CEO
 	private static int ceoID;
 	
-	//Map to represent the hierarchy of data into a Adjacency List of relationships (Manager-to-Employee)
+	//Map to represent the hierarchy of data into a Adjacency List of relationships (Manager-to-Employee).
 	private static Map<Integer, List<Integer>> map;
 	
-    public static void main( String[] args )
+	//To store the list of Managers whose salary is less than expected  
+	private static List<Employee> managerLessThanExptSalary;
+	
+	//To store the list of Managers whose salary is more than expected
+	private static List<Employee> managerMoreThanExptSalary;
+	
+	//To store the list of Employee who is having lengthier reports
+	private static List<Employee> empsMoreThanExptReports;
+	
+	//Entry point of the application
+    public static void main(String[] args)
     {
     	//Method to read the csv raw data 
     	readCSV();
     	
-    	//Represent the data in a form so we can compute the queries easily
+    	//Represent the data in a form/structure so we can compute the queries easily
     	representHiararchy();
     	
+    	//Enhance the data to address the query requirements
     	enhanceEmployeeData();
+    	
+    	//Queries
+    	printManagerMoreThanExptSalary();
+    	printManagerLessThanExptSalary();
+    	printEmpsMoreThanExptReports();
     }
     
-    private static void enhanceEmployeeData() {
-		
+	private static void printManagerMoreThanExptSalary() {
+		System.out.println("\nManagers having MORE than Expected Salary");
+		for(Employee e: managerMoreThanExptSalary)
+			System.out.println(e.toString());
+	}
+
+	private static void printManagerLessThanExptSalary() {
+		System.out.println("\nManagers having LESS than Expected Salary");
+		for(Employee e: managerLessThanExptSalary)
+			System.out.println(e.toString());
+	}
+	
+	private static void printEmpsMoreThanExptReports() {
+		System.out.println("\nEmployees having Lengthier Reporting");
+		for(Employee e: empsMoreThanExptReports)
+			System.out.println(e.toString());
+	}
+	
+	/**
+	 * This method is used to traverse through the hierarchy starting from CEO (root). 
+	 * Captures their salary comparison outcome
+	 * Capture their distances 
+	 */
+	private static void enhanceEmployeeData() {
+		//Initialize distance list, to track how far each empooyee from CEO
+    	Map<Integer, Integer> distanceMap = new HashMap<>();
+    	
+    	managerMoreThanExptSalary = new ArrayList<>();
+    	managerLessThanExptSalary = new ArrayList<>();
+    	empsMoreThanExptReports = new ArrayList<>();
+    	
+    	Deque<Integer> q = new ArrayDeque<>();
+    	q.add(ceoID);
+    	distanceMap.put(ceoID, 0);
+    	
+    	while(!q.isEmpty()) {
+    		int curEmployee = q.pollFirst();
+    		List<Integer> reportees = map.get(curEmployee);
+    		if(reportees != null && !reportees.isEmpty()) {
+    			double sumOfReporteesSalary = 0;
+    			for(int r: reportees) {
+    				sumOfReporteesSalary += empMap.get(r).getSalary();
+    				
+    				//Calculate distance from CEO
+    				int dist = distanceMap.get(curEmployee) + 1;
+    				distanceMap.put(r, dist);
+    				
+    				//Now lets ad this employee to back of the queue
+    				q.add(r);
+    				
+    				//If the distance of this Employee is already more than expected reporting length, capture and store into final list
+    				if(dist > CONFIG_MAX_REPORTEES)
+    					empsMoreThanExptReports.add(empMap.get(r));
+    			}
+    			
+    			//Now lets check the Managers's salary in terms of comparision with their reportees average
+    			double avg = sumOfReporteesSalary / reportees.size(); //Calculate average of reportees
+    			
+    			double curEmpSalary = empMap.get(curEmployee).getSalary();
+    			double salaryRatio = (curEmpSalary/avg)*100 - 100; //Percentage difference
+    			
+    			if(salaryRatio < CONFIG_MIN_PRCT)
+    				managerLessThanExptSalary.add(empMap.get(curEmployee));
+    			else if(salaryRatio > CONFIG_MAX_PRCT)
+    				managerMoreThanExptSalary.add(empMap.get(curEmployee));
+    		}
+    	}
 	}
 
 	private static void representHiararchy() {
     	
     	map = new HashMap<>();
+    	empMap = new HashMap<>();
 		
 		//Populate the map
 		for(Employee e : rawData) {
@@ -56,7 +149,7 @@ public class App
 			//CEO Case
 			if(e.getManagerId() == 0) {
 				ceoID = e.getId();
-				map.put(e.getId(), new ArrayList<>());
+				map.put(e.getId(), null);
 			}
 			else {
 				List<Integer> reportees = map.get(e.getManagerId());
@@ -71,7 +164,7 @@ public class App
 
 	private static void readCSV() {
     	try {
-			FileReader fileReader = new FileReader("D:\\STS_WS\\organization-service\\src\\main\\java\\com\\swissre\\organizationservice\\company_org_structure.csv");
+			FileReader fileReader = new FileReader(CONFIG_CSV_PATH);
 			CSVReader csvReader = new CSVReader(fileReader);
 			
 			CsvToBean<Employee> csvToBean = new CsvToBeanBuilder<Employee>(csvReader)
@@ -82,7 +175,7 @@ public class App
 			rawData = csvToBean.parse();
 						
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} 
+			System.err.println("File Not Found, please check the path and re-execute the application!!");
+		}
     }
 }
